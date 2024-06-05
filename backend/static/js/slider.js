362b5c836516726703c8722dbc3c79241f89667d
Ilaria Coletti <ilaria@lupanarium>
2024-06-05 17:10:25 +0200

    slider in js

---
+++ backend/static/js/slider.js
@@ -0,0 +1,7 @@
+function updateQuantityValue(val) {
+    document.getElementById('quantity-input').value = val;
+}
+
+function updateSliderValue(val) {
+    document.getElementById('quantity-slider').value = val;
+}
