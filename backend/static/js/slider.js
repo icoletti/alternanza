2d730413c29cafd0f7bc0d945eb5498e5bc27f13
Ilaria Coletti <ilaria@lupanarium>
2024-06-12 09:01:31 +0200

    file js script

--- backend/static/js/slider.js
+++ backend/static/js/slider.js
@@ -5,3 +5,39 @@ function updateQuantityValue(val) {
 function updateSliderValue(val) {
     document.getElementById('quantity-slider').value = val;
 }
+
+function selectOption(optionId) {
+    var option = document.getElementById(optionId);
+    option.checked = true;
+    toggleSubOperations();
+}
+
+function toggleSubOperations() {
+    var mainElement = document.querySelector('input[name="operation"]:checked');
+    var standardSubOptions = document.getElementById('sub-options-standard');
+    var bestSubOptions = document.getElementById('sub-options-best');
+    var newFieldElement = document.querySelector('input[name="invito_boolean"]:checked');
+    var newSubField = document.getElementById('new-sub-field');
+
+    if (!mainElement) {
+        standardSubOptions.classList.add('hidden');
+        bestSubOptions.classList.add('hidden');
+    } else if (mainElement.value === 'standard') {
+        standardSubOptions.classList.remove('hidden');
+        bestSubOptions.classList.add('hidden');
+    } else if (mainElement.value === 'best_option0') {
+        bestSubOptions.classList.remove('hidden');
+        standardSubOptions.classList.add('hidden');
+    } else {
+        standardSubOptions.classList.add('hidden');
+        bestSubOptions.classList.add('hidden');
+    }
+
+    if (newFieldElement && newFieldElement.value === 'best_option') {
+        newSubField.classList.remove('hidden');
+    } else {
+        newSubField.classList.add('hidden');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', toggleSubOperations);
